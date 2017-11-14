12972685835684967e659017338ce33e072b2dae
IB-2015 <1522411@stud.hs-mannheim.de>
2017-11-14 21:29:03 +0100

    about_exceptions.rb: 156 of 282

--- koans/about_exceptions.rb
+++ koans/about_exceptions.rb
@@ -6,10 +6,12 @@ class AboutExceptions < Neo::Koan
   end
 
   def test_exceptions_inherit_from_Exception
-    assert_equal __, MySpecialError.ancestors[1]
-    assert_equal __, MySpecialError.ancestors[2]
-    assert_equal __, MySpecialError.ancestors[3]
-    assert_equal __, MySpecialError.ancestors[4]
+    # MySpecialError.ancestors =>
+    # MM, RuntimeError, StandardError, Exception, Object, Kernel, BasicObject
+    assert_equal RuntimeError, MySpecialError.ancestors[1]
+    assert_equal StandardError, MySpecialError.ancestors[2]
+    assert_equal Exception, MySpecialError.ancestors[3]
+    assert_equal Object, MySpecialError.ancestors[4]
   end
 
   def test_rescue_clause
