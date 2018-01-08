6506614edbf2e1cfd8fe304363589410ad1964cc
IB-2015 <1522411@stud.hs-mannheim.de>
2018-01-08 23:06:11 +0100

    encrypt works; decrypt TODO

--- crypter/crypto.c
+++ crypter/crypto.c
@@ -1,53 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "crypto.h"
-
-int length(const char* s) {
-  int length = 0;
-  while(s[length++] != '\0');
-  return length - 1;
-}
 
 int encrypt(KEY key, const char* input, char* output) {
   int i = 0;
-  int key_length = length(key.chars);
+  int key_length = strlen(key.chars);
   // output = (char*) malloc(length(input));
-  printf("%d     %d\n", key_length, length(input));
   while(input[i] != '\0') {
     output[i] = ((input[i] - 'A' + 1) ^ (key.chars[i % key_length] - 'A' + 1)) + 'A' - 1;
     i++;
   }
-  printf("in: %s\n", output);
+}
+
+int decrypt(KEY key, const char* cypherText, char* output) {
+  int i = 0;
+  int key_length = strlen(key.chars);
+
+  while(cypherText[i] != '\0') {
+    output[i] = ((cypherText[i] + 'A' - 1) ^ (key.chars[i % key_length] + 'A' - 1)) - 'A' + 1;
+    i++;
+  }
+  return 0;
 }
 
 int main(int argc, char const *argv[]) {
-  // int a = 1;
-  // int b = 1;
-  // int c = a ^ b;
-  // char s[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  // char k[] = "TPERULES";
-
-  // printf("a: %d\n", a);
-  // printf("b: %d\n", b);
-  // printf("c: %d\n", c);
-
-  // int i = 0;
-  // while(s[i] != '\0') {
-  //   printf("original => %2d, %c; key => %2d, %c; crypt => %2d, %c\n",
-  //                                                 s[i] - 'A' + 1, s[i],
-  //                                                 k[i % length(k)] - 'A' + 1, k[i % length(k)],
-  //                                                 (s[i] - 'A' + 1) ^ (k[i % length(k)] - 'A' + 1), ((s[i] -'A' + 1) ^ (k[i % length(k)] - 'A' + 1)) + 'A' - 1);
-  //   i++;
-  // }
-
-  // printf("original: %d; key: %d\n", length(s), length(k));
-
+  int i = 0;
   KEY key;
+  while(i < argc)
+    printf("%s", argv[i++]);
+  printf("\n");
+
   char* input = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  printf("size %d  length %d\n", sizeof input, length(input));
-  char* output = (char*) malloc(length(input));
+  char* output = (char*) malloc(strlen(input) + 1);
   key.chars = "TPERULES";
   encrypt(key, input, output);
-  printf("ou: %s\n", output);
+  printf("original  : %s\n", input);
+  printf("key       : ");
+  i = 0;
+  while(input[i] != '\0')
+    printf("%c", key.chars[i++ % strlen(key.chars)]);
+  printf("\n");
+  printf("output    : %s\n", output);
+
+  char* cypherText = (char*) malloc(strlen(output) + 1);
+  strncpy(cypherText, output, strlen(output));
+  free(output);
+  output = NULL;
+  output = (char*) malloc(strlen(cypherText) + 1);
+  decrypt(key, cypherText, output);
+  printf("cypherText: %s\n", cypherText);
+  printf("key       : ");
+  i = 0;
+  while(input[i] != '\0')
+    printf("%c", key.chars[i++ % strlen(key.chars)]);
+  printf("\n");
+  printf("output    : %s\n", output);
   return 0;
 }
